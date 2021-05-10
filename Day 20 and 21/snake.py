1688f9ee85074fd310ecaf48806457ff9d8bf293
neonritual <vtshore@gmail.com>
2021-05-10 22:02:16 +0900

    day 21 finishing snake game

--- Day 20 and 21/snake.py
+++ Day 20 and 21/snake.py
@@ -16,11 +16,19 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.penup()
-            new_segment.color("white")
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.penup()
+        new_segment.speed("fastest")
+        new_segment.color("white")
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        #add new segment to snake
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):  # start=, stop=0 , step=-1
