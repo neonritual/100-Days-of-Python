2954cb468a049cc4cd769922a9a32a6d073b1b46
neonritual <vtshore@gmail.com>
2021-05-20 22:54:17 +0900

    Day 27, playground

---
+++ Day 26/Day 27/playground.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+#Challenge: make a function called add(), that will always add ALL the arguments within it.
+#
+# def add(*args):
+#     answer = sum(args)
+#     print(answer)
+
+# add(50, 50, 50, 50, 50, 50)
