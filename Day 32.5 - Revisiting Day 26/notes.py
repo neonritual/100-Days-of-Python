6781231e7ac8c50df304872381efea10e65c4a1b
neonritual <vtshore@gmail.com>
2021-05-29 22:43:26 +0900

    Day 26 Redo part 2

--- Day 32.5 - Revisiting Day 26/notes.py
+++ Day 32.5 - Revisiting Day 26/notes.py
@@ -45,4 +45,68 @@
 # new_list = [int(i) for i in file1_list if i in file2_list]
 # print(new_list)
 
+#---------------------------------------------------------------------#
+#DICTIONARY COMPREHENSION
+
+# new_dict = {new_key:new_key for (key, value) in dict.items() if test}
+
+#challenge: randomly assign student scores, then make dict of passing grades
+# import random
+# names = ["Arren", "Ilbraden", "Elq", "Yi"] #list of student names
+#
+# students = {student: random.randint(1, 100) for student in names} #gens a random 'score' for each student
+#
+# #create a dict of only students who have a passing score (over or equal 60)
+# passed_students = {student: score for (student, score) in students.items() if score >= 60}
+# print(passed_students)
+
+#------
+
+#Challenge: make a dict called result that calcs the length of each word in a given sentence.
+
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+#
+# result = {word: len(word) for word in sentence.split()}
+# print(result)
+
+
+#------
+
+#Challenge: takes celcius temps in dict and converts to temps in F.
+#
+# weather_c = {
+#     "Sunday": 24,
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+# }
+#
+# # weather_f = {day: ((temp * 9/5) + 32) for (day,temp) in weather_c.items()}
+# print(weather_f)
+
+#-----------
+#How to iterate over pandas dataframe
+
+student_dict = {
+    "student": ["Angel", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+import pandas as pd
+student_data_frame = pd.DataFrame(student_dict)
+print(student_data_frame)
+
+#Loop thru dataframe:
+
+# for (key, value) in student_data_frame.items():
+#     print(value)
+
+#loop thru rows of dataframe
+#
+# for (index, row) in student_data_frame.iterrows():
+#     if (row.student) =="Angel":
+#         blah blah
 
