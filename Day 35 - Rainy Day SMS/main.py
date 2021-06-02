303a978a4d20671086b71ac8a95f8986449b0fe8
neonritual <vtshore@gmail.com>
2021-06-02 22:55:55 +0900

    Day 35 Rainy Day SMS

---
+++ Day 35 - Rainy Day SMS/main.py
@@ -0,0 +1,43 @@
+import requests
+from twilio.rest import Client
+
+
+account_sid = "AC217bec6b32ed3add88134cb60dedc2ed"
+auth_token = "fa5de62aed9c584b14020017d8ecfb8f"
+API_KEY = "53587566147d448b18222a843f2cbb21"
+MY_LAT = **INSERT LAT HERE
+MY_LONG = **INSERT LONG HERE
+PART = "current,minutely,daily,alerts"
+TRILIO_NUMBER = "+18568884611"
+MY_NUMBER = ""
+
+
+
+response = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LAT}&lon={MY_LONG}&"
+                        f"exclude={PART}&appid={API_KEY}&unites=metric")
+
+response.raise_for_status()
+data = response.json()
+
+print(data["hourly"][0]["weather"][0]["id"])
+
+hours_list = []
+will_rain = False
+
+for hour in range(0, 12):
+    hours_list.append(data["hourly"][hour]["weather"][0]["id"])
+
+for x in hours_list:
+    if x < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="Looks like rain. Better bring an umbrella!",
+        from_= TRILIO_NUMBER,
+        to=MY_NUMBER
+    )
+    print(message.status)
+
