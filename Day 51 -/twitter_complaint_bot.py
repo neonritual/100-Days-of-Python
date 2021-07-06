381336da465490f080fa2b0080d930a50674245f
neonritual <vtshore@gmail.com>
2021-07-06 20:43:50 +0900

    Day 51 - Twitter complaint bot - comments

--- Day 51 -/twitter_complaint_bot.py
+++ Day 51 -/twitter_complaint_bot.py
@@ -1,17 +1,17 @@
 ### CHALLENGE: Make a Twitter bot to tweet at a company when your computer's speed test result is below
 # their guarenteed speeds.
-# Since I did not want to actually tweet at a company even as a test, I instead tweeted at my own account.
+# Since I did not want to actually tweet at a company even as a test, so this just tweets without @.
 
 from selenium import webdriver
 import time
 
-# These are the guarenteed upload and download speeds of my fictional internet company.
+# These are the guaranteed upload and download speeds of my fictional internet company.
 PROMISED_DOWN = 100.00
 PROMISED_UP = 10.00
 TWITTER_USERNAME = ""
 TWITTER_PASSWORD = ""
 chrome_driver_path = "/Users/valerie/Documents/chromedriver"
-#
+
 
 
 
@@ -24,7 +24,7 @@ class InternetSpeedBot():
 
 
     def tweet(self):
-
+        #Log in to twitter.
         self.driver.get("https://www.twitter.com")
         time.sleep(4)
         login_button = self.driver.find_element_by_link_text("Log in")
@@ -44,9 +44,7 @@ class InternetSpeedBot():
 
         time.sleep(2)
 
-        print(self.down_speed)
-        print(self.up_speed)
-
+        # Locate the tweet input field, and tweet using recently grabbed variables.
         tweet_field = self.driver.find_element_by_xpath(
             '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div'
                                                         )
@@ -59,6 +57,7 @@ class InternetSpeedBot():
         send_tweet_button.click()
 
     def get_internet_speed(self):
+        # Run an internet speed test and grab the up/down speeds.
         self.driver.get("https://www.speedtest.net")
         time.sleep(5)
         go_button = self.driver.find_element_by_class_name("start-text")
@@ -72,6 +71,8 @@ class InternetSpeedBot():
             '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span'
         ).text
 
+        # Compare the grabbed speeds to the speeds promised by my ISP; if lower than advertised,
+        # tweet.
         if float(self.up_speed) < PROMISED_UP or float(self.down_speed) < PROMISED_DOWN:
             self.tweet()
 
