07b55cad829b587685603a08f0887183fc381e03
neonritual <vtshore@gmail.com>
2021-07-08 20:59:35 +0900

    Day 53 - Zillow search info collector; only file renames

--- Day 31-40/Day 39 - Flight Tracker/flight-deals-start/secrets.py
+++
@@ -1,7 +0,0 @@
-TEQUILA_API = "Oq3SFYfhb3n0vKpVnWWy-NB0IkeI5B8g"
-TWILIO_ACCOUNT_SID = "AC217bec6b32ed3add88134cb60dedc2ed"
-TWILIO_AUTH_TOKEN = "4640f42929daad1a5fd1c59c45ab7fce"
-sheety_endpoint = "https://api.sheety.co/d5cfdf17a0678b4385c3ea18217713f8/copyOfFlightDeals/prices"
-bearer = "Bearer please"
-to_number = "+8108018687124"
-from_number = "+18568884611"
